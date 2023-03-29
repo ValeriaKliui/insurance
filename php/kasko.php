3cb9ad6d3669a95253af2a63fcccf41aa5825132
Valeria Kliui <valeriakliui@gmail.com>
2023-03-29 21:08:46 +0300

    add program to db

--- php/kasko.php
+++ php/kasko.php
@@ -1,12 +1,13 @@
 <?php
-if (isset($_POST["username"]) && isset($_POST["password"])) {
+if (isset($_POST["cost"])) {
     $conn = new mysqli("localhost", "root", "root", "testdb3");
     if($conn->connect_error){
         die("Ошибка: " . $conn->connect_error);
     }
-    $name = $conn->real_escape_string($_POST["username"]);
-    $password = $conn->real_escape_string($_POST["password"]);
-    $sql = "INSERT INTO users (name, password) VALUES ('$name', '$password')";
+    $program = $conn->real_escape_string(stristr($_POST["cost"], ' ', true));
+    $cost = $conn->real_escape_string(stristr($_POST["cost"], ' '));
+    $type = "Kasko";
+    $sql = "INSERT INTO insurance (program, cost, type) VALUES ('$program', '$cost', '$type')";
     if($conn->query($sql)){
         echo "Данные успешно добавлены";
     } else{
--- src/modules/kasko.js
+++ src/modules/kasko.js
@@ -9,9 +9,9 @@ const form = document.querySelector('.calculation-form');
 
 let allValues = [];
 if (allCosts) {
-for (let cost of allCosts){
-    allValues.push(+cost.outerText);
-}
+    for (let cost of allCosts) {
+        allValues.push(+cost.outerText);
+    }
 }
 let min = Math.min(...allValues);
 let max = Math.max(...allValues);
@@ -22,87 +22,86 @@ let from1_500 = document.querySelector('.cost_500_from1');
 let from2_500 = document.querySelector('.cost_500_from2');
 let full = document.querySelector('.cost_full');
 
-if (pragmatic) {
-let pragmaticVal = +document.querySelector('.cost_pragmatic').outerText;
+function calculate() {
+    let pragmaticVal = +document.querySelector('.cost_pragmatic').outerText;
 let optimVal = +document.querySelector('.cost_optim').outerText;
 let from1_500Val = +document.querySelector('.cost_500_from1').outerText;
 let from2_500Val = +document.querySelector('.cost_500_from2').outerText;
 let fullVal = +document.querySelector('.cost_full').outerText;
-}
-
-function calculate() {
     showTheCost();
-    form.addEventListener('change', function() {
-        checkYear(yearInput.value);
-        calculateProgmatic(costInput.value);
-        calculateOptim(costInput.value);
+    form.addEventListener('change', function () {
+        checkYear(yearInput.value, optimVal, from1_500Val, from2_500Val, fullVal);
+        calculateProgmatic(costInput.value, pragmaticVal);
+        calculateOptim(costInput.value, optimVal);
         allValues = [];
-        for (let cost of allCosts){
+        for (let cost of allCosts) {
             allValues.push(+cost.outerText);
         }
-         min = Math.min(...allValues);
-         max = Math.max(...allValues);
-        showTheCost(min, max);  
-              selectProgram();
+        min = Math.min(...allValues);
+        max = Math.max(...allValues);
+        showTheCost(min, max);
+        selectProgram();
     });
 }
-function checkYear(year) {
+
+function checkYear(year, optimVal, from1_500Val, from2_500Val, fullVal) {
     if (year) {
-    if (+year<2015) {
-        optim.innerHTML = `${optimVal-50}`;
-        from1_500.innerHTML = `${from1_500Val-100}`;
-        from2_500.innerHTML = `${from2_500Val-100}`;
-        full.innerHTML = `${fullVal-150}`;
-    }
-    else {
-        optim.innerHTML = `${optim.inner}`;
-        from1_500.innerHTML = `${from1_500Val}`;
-        cost500From2.innerHTML = `${cost500From2Val}`;
-        full.innerHTML = `${fullVal}`;
+        if (+year < 2015) {
+            optim.innerHTML = `${optimVal - 50}`;
+            from1_500.innerHTML = `${from1_500Val - 100}`;
+            from2_500.innerHTML = `${from2_500Val - 100}`;
+            full.innerHTML = `${fullVal - 150}`;
+        }
+        else {
+            optim.innerHTML = `${optim.inner}`;
+            from1_500.innerHTML = `${from1_500Val}`;
+            cost500From2.innerHTML = `${cost500From2Val}`;
+            full.innerHTML = `${fullVal}`;
+        }
     }
 }
-}
-function showTheCost(changedMin=min, changedMax=max, chosen) {
+function showTheCost(changedMin = min, changedMax = max, chosen) {
     finalCost.innerHTML = `$${changedMin}-$${changedMax}`;
 }
-function calculateProgmatic(cost){
+function calculateProgmatic(cost, pragmaticVal) {
     let initial = 7000;
     if (cost > initial) {
-        let procent = (cost-initial)/10000+1;
-        cost = Math.round(procent*pragmaticVal);
+        let procent = (cost - initial) / 10000 + 1;
+        cost = Math.round(procent * pragmaticVal);
         pragmatic.innerHTML = `${cost}`;
     }
 }
-function calculateOptim(cost){
+function calculateOptim(cost, optimVal) {
     let initial = 12700;
     if (cost > initial) {
-        let procent = (cost-initial)/10000+1-0.6;
-        cost = Math.round(procent*optimVal);
+        let procent = (cost - initial) / 10000 + 1 - 0.6;
+        cost = Math.round(procent * optimVal);
         optim.innerHTML = `${cost}`;
     }
 }
 
-function selectProgram(){
-    const programs = document.querySelectorAll('input[name="choice"]');
+function selectProgram() {
+    const programs = document.querySelectorAll('input[name="cost"]');
     for (const program of programs) {
-      if (program.checked) {
-        finalCost.innerHTML = '$' + document.querySelector(`.cost_${program.id}`).innerText;
-      }
+        if (program.checked) {
+            finalCost.innerHTML = '$' + document.querySelector(`.cost_${program.id}`).innerText;
+        }
     }
 }
 
 let buttonKasko = document.querySelector('.kasko_submit');
 
 if (buttonKasko) {
-buttonKasko.onclick = function() {
-    if (checkWhoOnline !=='Admin') {
-        const programs = document.querySelectorAll('input[name="choice"]');
-        for (const program of programs) {
-          if (program.checked) {
-    localStorage.setItem('program', program.id);
-          }
+    buttonKasko.onclick = function () {
+        if (checkWhoOnline !== 'Admin') {
+            const programs = document.querySelectorAll('input[name="choice"]');
+            for (const program of programs) {
+                if (program.checked) {
+                    program.value = program.value + ' ' + document.querySelector(`.cost_${program.id}`).innerText;
+                    localStorage.setItem('program', program.id);
+                }
+            }
+        }
     }
 }
-}
-}
-export {calculate}
+export { calculate }
